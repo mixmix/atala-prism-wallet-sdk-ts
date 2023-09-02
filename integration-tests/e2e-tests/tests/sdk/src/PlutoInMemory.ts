0d64801a0cca884b8e0dfcb041fcb17172d7f635
Allain Magyar <allain.magyar@iohk.io>
2023-09-01 23:01:58 -0300

    test: refactor and add new scenarios

--- integration-tests/e2e-tests/tests/sdk/src/PlutoInMemory.ts
+++ integration-tests/e2e-tests/tests/sdk/src/PlutoInMemory.ts
@@ -1,4 +1,4 @@
-import { Domain } from "@input-output-hk/atala-prism-wallet-sdk";
+import { Domain } from "@input-output-hk/atala-prism-wallet-sdk"
 
 interface DIDRecord {
   did: Domain.DID;
@@ -19,12 +19,12 @@ type MediatorRecord = Domain.Mediator;
 type CredentialRecord = Domain.VerifiableCredential;
 
 export class PlutoInMemory implements Domain.Pluto {
-  _didStorage: Record<string, DIDRecord> = {};
-  _keyStorage: Record<string, Array<KeyRecord>> = {};
-  _didPairStorage: Array<DIDPairRecord> = [];
-  _messageStorage: Array<MessageRecord> = [];
-  _mediatorStorage: Array<MediatorRecord> = [];
-  _credentialStorage: Array<CredentialRecord> = [];
+  _didStorage: Record<string, DIDRecord> = {}
+  _keyStorage: Record<string, Array<KeyRecord>> = {}
+  _didPairStorage: Array<DIDPairRecord> = []
+  _messageStorage: Array<MessageRecord> = []
+  _mediatorStorage: Array<MediatorRecord> = []
+  _credentialStorage: Array<CredentialRecord> = []
 
   /**
    * no async setup for in-memory implementation
@@ -32,50 +32,50 @@ export class PlutoInMemory implements Domain.Pluto {
   async start() {}
 
   async destroy() {
-    this._didStorage = {};
-    this._keyStorage = {};
-    this._didPairStorage = [];
-    this._messageStorage = [];
-    this._mediatorStorage = [];
-    this._credentialStorage = [];
+    this._didStorage = {}
+    this._keyStorage = {}
+    this._didPairStorage = []
+    this._messageStorage = []
+    this._mediatorStorage = []
+    this._credentialStorage = []
   }
 
   async storePrismDID(
-      did: Domain.DID,
-      keyPathIndex: number,
-      privateKey: Domain.PrivateKey,
-      privateKeyMetaId: string | null,
-      alias?: string,
+    did: Domain.DID,
+    keyPathIndex: number,
+    privateKey: Domain.PrivateKey,
+    privateKeyMetaId: string | null,
+    alias?: string,
   ) {
-    const didStr = did.toString();
+    const didStr = did.toString()
     await this.storePrivateKeys(
-        privateKey,
-        did,
-        keyPathIndex,
-        privateKeyMetaId,
-    );
+      privateKey,
+      did,
+      keyPathIndex,
+      privateKeyMetaId,
+    )
     this._didStorage[didStr] = {
       did,
       alias,
-    };
+    }
   }
 
   async storePeerDID(did: Domain.DID, privateKeys: Array<Domain.PrivateKey>) {
-    const didStr = did.toString();
+    const didStr = did.toString()
     this._keyStorage[didStr] = privateKeys.map((privateKey) => ({
       did,
       privateKey,
       keyPathIndex: undefined,
       privateKeyMetaId: undefined,
-    }));
+    }))
     this._didStorage[didStr] = {
       did,
       alias: undefined,
-    };
+    }
   }
 
   async storeDIDPair(host: Domain.DID, receiver: Domain.DID, name: string) {
-    this._didPairStorage.push({ host, receiver, name });
+    this._didPairStorage.push({ host, receiver, name })
   }
 
   async storeMessage(message: Domain.Message) {
@@ -83,25 +83,25 @@ export class PlutoInMemory implements Domain.Pluto {
       message,
       sourceType:
           message.direction === Domain.MessageDirection.SENT
-              ? "sent"
-              : "received",
-    });
+            ? "sent"
+            : "received",
+    })
   }
 
   async storeMessages(messages: Array<Domain.Message>) {
-    messages.forEach((message) => this.storeMessage(message));
+    messages.forEach((message) => this.storeMessage(message))
   }
 
   async storePrivateKeys(
-      privateKey: Domain.PrivateKey,
-      did: Domain.DID,
-      keyPathIndex: number,
-      metaId: string | null,
+    privateKey: Domain.PrivateKey,
+    did: Domain.DID,
+    keyPathIndex: number,
+    metaId: string | null,
   ) {
-    const didStr = did.toString();
+    const didStr = did.toString()
 
     if (!this._keyStorage[didStr]) {
-      this._keyStorage[didStr] = [];
+      this._keyStorage[didStr] = []
     }
 
     this._keyStorage[didStr].push({
@@ -109,240 +109,240 @@ export class PlutoInMemory implements Domain.Pluto {
       privateKey,
       keyPathIndex,
       privateKeyMetaId: metaId || undefined,
-    });
+    })
   }
 
   async storeMediator(
-      mediator: Domain.DID,
-      host: Domain.DID,
-      routing: Domain.DID,
+    mediator: Domain.DID,
+    host: Domain.DID,
+    routing: Domain.DID,
   ) {
     this._mediatorStorage.push({
       mediatorDID: mediator,
       hostDID: host,
       routingDID: routing,
-    });
+    })
   }
 
   async storeCredential(credential: Domain.VerifiableCredential) {
-    this._credentialStorage.push(credential);
+    this._credentialStorage.push(credential)
   }
 
   async getAllPrismDIDs(): Promise<Domain.PrismDIDInfo[]> {
     return Object.values(this._didStorage)
-        .filter((didRecord) => didRecord.did.method === "prism")
-        .map((didRecord) => {
-          const keyRecord = this._keyStorage[didRecord.did.toString()][0];
-          return new Domain.PrismDIDInfo(
-              didRecord.did,
-              keyRecord.keyPathIndex || 0,
-              didRecord.alias,
-          );
-        });
+      .filter((didRecord) => didRecord.did.method === "prism")
+      .map((didRecord) => {
+        const keyRecord = this._keyStorage[didRecord.did.toString()][0]
+        return new Domain.PrismDIDInfo(
+          didRecord.did,
+          keyRecord.keyPathIndex || 0,
+          didRecord.alias,
+        )
+      })
   }
 
   async getDIDInfoByDID(did: Domain.DID): Promise<Domain.PrismDIDInfo | null> {
-    const didRecord = this._didStorage[did.toString()];
+    const didRecord = this._didStorage[did.toString()]
 
     if (!didRecord) {
-      return null;
+      return null
     }
 
-    const keyRecord = this._keyStorage[didRecord.did.toString()][0];
+    const keyRecord = this._keyStorage[didRecord.did.toString()][0]
     return new Domain.PrismDIDInfo(
-        didRecord.did,
-        keyRecord.keyPathIndex || 0,
-        didRecord.alias,
-    );
+      didRecord.did,
+      keyRecord.keyPathIndex || 0,
+      didRecord.alias,
+    )
   }
 
   async getDIDInfoByAlias(alias: string): Promise<Domain.PrismDIDInfo[]> {
     return Object.values(this._didStorage)
-        .filter((didRecord) => didRecord.alias === alias)
-        .map((didRecord) => {
-          const keyRecord = this._keyStorage[didRecord.did.toString()][0];
-          return new Domain.PrismDIDInfo(
-              didRecord.did,
-              keyRecord.keyPathIndex || 0,
-              didRecord.alias,
-          );
-        });
+      .filter((didRecord) => didRecord.alias === alias)
+      .map((didRecord) => {
+        const keyRecord = this._keyStorage[didRecord.did.toString()][0]
+        return new Domain.PrismDIDInfo(
+          didRecord.did,
+          keyRecord.keyPathIndex || 0,
+          didRecord.alias,
+        )
+      })
   }
 
   async getPrismDIDKeyPathIndex(did: Domain.DID): Promise<number | null> {
-    const didRecord = this._didStorage[did.toString()];
+    const didRecord = this._didStorage[did.toString()]
 
     if (!didRecord) {
-      return null;
+      return null
     }
 
-    const keyRecord = this._keyStorage[didRecord.did.toString()][0];
-    return keyRecord.keyPathIndex || 0;
+    const keyRecord = this._keyStorage[didRecord.did.toString()][0]
+    return keyRecord.keyPathIndex || 0
   }
 
   async getPrismLastKeyPathIndex(): Promise<number> {
-    const allPrismDIDs = await this.getAllPrismDIDs();
+    const allPrismDIDs = await this.getAllPrismDIDs()
     const allKeyPathIndexes = allPrismDIDs.map(
-        (didInfo) => didInfo.keyPathIndex,
-    );
+      (didInfo) => didInfo.keyPathIndex,
+    )
 
     if (allKeyPathIndexes.length === 0) {
-      return 0;
+      return 0
     }
 
-    return Math.max(...allKeyPathIndexes);
+    return Math.max(...allKeyPathIndexes)
   }
 
   async getAllPeerDIDs() {
     const allPeerDIDs = Object.values(this._didStorage).filter(
-        (didRecord) => didRecord.did.method === "peer",
-    );
+      (didRecord) => didRecord.did.method === "peer",
+    )
 
     const allPeerDIDsWithKeys = allPeerDIDs.map((didRecord) => {
-      const keyRecords = this._keyStorage[didRecord.did.toString()];
+      const keyRecords = this._keyStorage[didRecord.did.toString()]
 
       const privateKeysForPeerDID = keyRecords.map((key) => ({
         keyCurve: Domain.getKeyCurveByNameAndIndex(
-            key.privateKey.curve,
-            key.keyPathIndex,
+          key.privateKey.curve,
+          key.keyPathIndex,
         ),
         value: key.privateKey.value,
-      }));
+      }))
 
-      return new Domain.PeerDID(didRecord.did, privateKeysForPeerDID);
-    });
+      return new Domain.PeerDID(didRecord.did, privateKeysForPeerDID)
+    })
 
-    return allPeerDIDsWithKeys;
+    return allPeerDIDsWithKeys
   }
   async getDIDPrivateKeysByDID(did: Domain.DID) {
-    const didStr = did.toString();
-    const keyRecords = this._keyStorage[didStr];
+    const didStr = did.toString()
+    const keyRecords = this._keyStorage[didStr]
 
     if (!keyRecords) {
-      return [];
+      return []
     }
 
-    return keyRecords.map((keyRecord) => keyRecord.privateKey);
+    return keyRecords.map((keyRecord) => keyRecord.privateKey)
   }
 
   async getDIDPrivateKeyByID(id: string): Promise<Domain.PrivateKey | null> {
     // TODO: it seems it's not used anywhere
-    const allKeys = Object.values(this._keyStorage).flat();
+    const allKeys = Object.values(this._keyStorage).flat()
     const keyRecord = allKeys.find(
-        (keyRecord) => keyRecord.privateKeyMetaId === id,
-    );
+      (keyRecord) => keyRecord.privateKeyMetaId === id,
+    )
 
-    return keyRecord?.privateKey ?? null;
+    return keyRecord?.privateKey ?? null
   }
 
   async getAllDidPairs() {
     // TODO: it seems it's not used anywhere
-    return this._didPairStorage;
+    return this._didPairStorage
   }
 
   async getPairByDID(did: Domain.DID): Promise<Domain.DIDPair | null> {
     return (
-        this._didPairStorage.find(
-            (pair) => pair.host.toString() === did.toString(),
-        ) || null
-    );
+      this._didPairStorage.find(
+        (pair) => pair.host.toString() === did.toString(),
+      ) || null
+    )
   }
 
   async getPairByName(name: string) {
-    return this._didPairStorage.find((pair) => pair.name === name) || null;
+    return this._didPairStorage.find((pair) => pair.name === name) || null
   }
 
   async getAllMessages() {
-    return this._messageStorage.map((messageRecord) => messageRecord.message);
+    return this._messageStorage.map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesByDID(did: Domain.DID): Promise<Array<Domain.Message>> {
     // get all messages where either from or to is equal did
     return this._messageStorage
-        .filter(
-            (messageRecord) =>
-                messageRecord.message.from?.toString() === did.toString() ||
+      .filter(
+        (messageRecord) =>
+          messageRecord.message.from?.toString() === did.toString() ||
                 messageRecord.message.to?.toString() === did.toString(),
-        )
-        .map((messageRecord) => messageRecord.message);
+      )
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesSent() {
     return this._messageStorage
-        .filter((messageRecord) => messageRecord.sourceType === "sent")
-        .map((messageRecord) => messageRecord.message);
+      .filter((messageRecord) => messageRecord.sourceType === "sent")
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesReceived(): Promise<Array<Domain.Message>> {
     return this._messageStorage
-        .filter((messageRecord) => messageRecord.sourceType === "received")
-        .map((messageRecord) => messageRecord.message);
+      .filter((messageRecord) => messageRecord.sourceType === "received")
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesSentTo(did: Domain.DID) {
     return this._messageStorage
-        .filter(
-            (messageRecord) =>
-                messageRecord.message.to?.toString() === did.toString(),
-        )
-        .map((messageRecord) => messageRecord.message);
+      .filter(
+        (messageRecord) =>
+          messageRecord.message.to?.toString() === did.toString(),
+      )
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesReceivedFrom(did: Domain.DID) {
     return this._messageStorage
-        .filter(
-            (messageRecord) =>
-                messageRecord.message.from?.toString() === did.toString(),
-        )
-        .map((messageRecord) => messageRecord.message);
+      .filter(
+        (messageRecord) =>
+          messageRecord.message.from?.toString() === did.toString(),
+      )
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesOfType(
-      type: string,
-      relatedWithDID?: Domain.DID,
+    type: string,
+    relatedWithDID?: Domain.DID,
   ): Promise<Array<Domain.Message>> {
     return this._messageStorage
-        .filter(
-            (messageRecord) =>
-                messageRecord.message.piuri === type &&
+      .filter(
+        (messageRecord) =>
+          messageRecord.message.piuri === type &&
                 (!relatedWithDID ||
                     messageRecord.message.from?.toString() ===
                     relatedWithDID.toString() ||
                     messageRecord.message.to?.toString() === relatedWithDID.toString()),
-        )
-        .map((messageRecord) => messageRecord.message);
+      )
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getAllMessagesByFromToDID(
-      from: Domain.DID,
-      to: Domain.DID,
+    from: Domain.DID,
+    to: Domain.DID,
   ): Promise<Array<Domain.Message>> {
     return this._messageStorage
-        .filter(
-            (messageRecord) =>
-                messageRecord.message.from?.toString() === from.toString() &&
+      .filter(
+        (messageRecord) =>
+          messageRecord.message.from?.toString() === from.toString() &&
                 messageRecord.message.to?.toString() === to.toString(),
-        )
-        .map((messageRecord) => messageRecord.message);
+      )
+      .map((messageRecord) => messageRecord.message)
   }
 
   async getMessage(id: string): Promise<Domain.Message | null> {
     const messageRecord = this._messageStorage.find(
-        (messageRecord) => messageRecord.message.id === id,
-    );
+      (messageRecord) => messageRecord.message.id === id,
+    )
 
     if (!messageRecord) {
-      return null;
+      return null
     }
 
-    return messageRecord.message;
+    return messageRecord.message
   }
 
   async getAllMediators(): Promise<Array<Domain.Mediator>> {
-    return this._mediatorStorage;
+    return this._mediatorStorage
   }
 
   async getAllCredentials(): Promise<Array<Domain.VerifiableCredential>> {
-    return this._credentialStorage;
+    return this._credentialStorage
   }
 }
--- integration-tests/e2e-tests/tests/sdk/steps/LifecycleSteps.ts
+++ integration-tests/e2e-tests/tests/sdk/steps/LifecycleSteps.ts
@@ -1,31 +1,28 @@
-import { After, Before, BeforeAll } from "@cucumber/cucumber"
+import { Before, BeforeAll } from "@cucumber/cucumber"
 import { Actor, Cast, TakeNotes, actorCalled, engage } from "@serenity-js/core"
 import { CallAnApi } from "@serenity-js/rest"
-import { EnvironmentVariables } from "../environment.variables"
-import { SdkAgentWorkflow } from "../src/SdkAgentWorkflow"
-import { PrismAgentWorkflow } from "../src/PrismAgentWorkflow"
+import { EnvironmentVariables } from "../EnvironmentVariables"
 import axios from "axios"
+import {Utils} from "../../Utils"
+import {WalletSdk} from "../WalletSdk"
+import {CloudAgentConfiguration} from "../configuration/CloudAgentConfiguration"
 
-export let axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: EnvironmentVariables.agentUrl,
   timeout: 10000,
   headers: {
-    Accept: 'application/json,application/xml',
+    Accept: "application/json,application/xml",
     APIKEY: EnvironmentVariables.apiKey
   }
 })
 
 BeforeAll(async() => {
-  await PrismAgentWorkflow.prepare()
+  Utils.prepareNotes()
+  await CloudAgentConfiguration.prepare()
 })
 
 Before(async() => {
-  await SdkAgentWorkflow.createAgent()
-  Actors.createAndEngageActors()
-})
-
-After(async() => {
-  await SdkAgentWorkflow.stop()
+  await Actors.createAndEngageActors()
 })
 
 class Actors implements Cast {
@@ -42,16 +39,17 @@ class Actors implements Cast {
     return this.actors.get(actor.name)!
   }
 
-  static createAndEngageActors() {
-    let actors = new Actors()
+  static async createAndEngageActors() {
+    const actors = new Actors()
 
-    let prismAgent = actorCalled("Cloud Agent").whoCan(
+    const prismAgent = actorCalled("Cloud Agent").whoCan(
       CallAnApi.using(axiosInstance),
       TakeNotes.usingAnEmptyNotepad()
     )
 
-    let edgeAgent = actorCalled("Edge Agent").whoCan(
-      TakeNotes.usingAnEmptyNotepad()
+    const edgeAgent = actorCalled("Edge Agent").whoCan(
+      TakeNotes.usingAnEmptyNotepad(),
+      await WalletSdk.withANewInstance()
     )
 
     actors.add(prismAgent)
